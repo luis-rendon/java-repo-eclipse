4e834634dc584b14f0a0611bf7f501e943af4215
lrendon <lrendon@192.168.11.110>
2016-04-27 15:56:19 -0700

    Project Created.

---
+++ src/com/inteltransform/gittest/HelloWorldWithGit.java
@@ -0,0 +1,7 @@
+package com.inteltransform.gittest;
+
+public class HelloWorldWithGit {
+    public static void main(String[] args) {
+        System.out.println("This my first hello world with Git");
+    }
+}
