88b6d84285b202fdcfd5e2c608cf90a4ba9d898f
lrendon <lrendon@192.168.11.110>
2016-04-28 12:08:02 -0700

    Added Change 3

--- src/com/inteltransform/gittest/HelloWorldWithGit.java
+++ src/com/inteltransform/gittest/HelloWorldWithGit.java
@@ -4,6 +4,8 @@ public class HelloWorldWithGit {
     public static void main(String[] args) {
         System.out.println("This my first hello world with Git");
         //Change 2
+        //Change 3
         //Change 4
+
     }
 }
